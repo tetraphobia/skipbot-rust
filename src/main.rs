b57e629628c705ee79c725de0ae8e489eafbc7a3
Jacob Teddy <tetraphobia@wyvrn.dev>
2023-04-20 12:46:23 -0400

    feat: add logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use fern::colors::{Color, ColoredLevelConfig};
 use modules::*;
 use poise::serenity_prelude as serenity;
+use std::time::SystemTime;
 
 mod modules;
 
@@ -7,8 +9,32 @@ pub struct Data {} // User data, which is stored and accessible in all command i
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
 pub type Context<'a> = poise::Context<'a, Data, Error>;
 
+fn setup_logger() {
+    let colors = ColoredLevelConfig::new().debug(Color::Magenta);
+
+    fern::Dispatch::new()
+        .chain(std::io::stdout())
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "[{} {} {}] {}",
+                humantime::format_rfc3339_seconds(SystemTime::now()),
+                // This will color the log level only, not the whole line. Just a touch.
+                colors.color(record.level()),
+                record.target(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Info)
+        .level_for("tracing::span", log::LevelFilter::Warn)
+        .level_for("serenity", log::LevelFilter::Warn)
+        .apply()
+        .unwrap();
+}
+
 #[tokio::main]
 async fn main() {
+    setup_logger();
+
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
             commands: development::get_module_commands(),
@@ -23,5 +49,6 @@ async fn main() {
             })
         });
 
+    log::info!("Skipbot framework initialized");
     framework.run().await.unwrap();
 }
--- src/modules/development/commands/ping.rs
+++ src/modules/development/commands/ping.rs
@@ -3,7 +3,7 @@ use crate::{Context, Error};
 /// Replies with "Pong."
 #[poise::command(slash_command, prefix_command)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
-    let response = "Pong.";
-    ctx.say(response).await?;
+    log::info!("Received ping command from {}", ctx.author().name);
+    ctx.say("Pong.").await?;
     Ok(())
 }
